9a054dde412fe8022713e242a936572805969215
caohf <919746894@qq.com>
2022-06-01 18:23:17 +0800

    add ant-design-vue

--- src/main.js
+++ src/main.js
@@ -1,7 +1,15 @@
 import Vue from 'vue'
+// import Antd from 'ant-design-vue'
 import App from './App.vue'
 
+import 'ant-design-vue/dist/antd.css'
+
 Vue.config.productionTip = false
+
+// Vue.use(Antd)
+// const app = createApp(App)
+
+// app.use(Antd).mount('#app')
 
 new Vue({
   render: h => h(App),
