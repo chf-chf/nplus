7a40e10344087896594fff00b1de7c453078867d
caohf <919746894@qq.com>
2022-06-06 14:25:03 +0800

    add vue-image-editor

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Antd from 'ant-design-vue'
+
 import App from './App.vue'
+import router from './router'
 
 import 'ant-design-vue/dist/antd.css'
 
@@ -9,5 +11,6 @@ Vue.config.productionTip = false
 Vue.use(Antd)
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
---
+++ src/pages/home/index.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="home">
+        <ImageEditor ref="tuiImageEditor" :include-ui="useDefaultUI" :options="options"></ImageEditor>
+    </div>
+</template>
+
+<script>
+    import "tui-image-editor/dist/tui-image-editor.css"
+    import "tui-color-picker/dist/tui-color-picker.css"
+
+    import ImageEditor from '@toast-ui/vue-image-editor/src/ImageEditor.vue'
+    import Logo from '@/assets/logo.png'
+    export default {
+        name: 'Home',
+        components: {
+            ImageEditor
+        },
+        data() {
+            return {
+                useDefaultUI: true,
+                options: {
+                    includeUI: {
+                        initMenu: 'draw',
+                        menu: [
+                            'crop',
+                            'draw',
+                            'icon',
+                            'text',
+                            'flip',
+                            'rotate',
+                            'shape',
+                            'mask',
+                            'filter'
+                        ],
+                        menuBarPosition: 'bottom',
+                        loadImage: {
+                            path: Logo,
+                            name: 'sample'
+                        }
+                    }
+                }
+            }
+        },
+        mounted() {
+            console.log(this.$refs.tuiImageEditor, '888')
+            const drawingMode = this.$refs.tuiImageEditor.invoke('getDrawingMode')
+            this.$refs.tuiImageEditor.invoke('loadImageFromURL', '../../assets/logo.png')
+            console.log(drawingMode, 'drawingMode')
+        }
+    }
+</script>
+
+<style scoped>
+    .home {
+        height: 100%;
+    }
+</style>
